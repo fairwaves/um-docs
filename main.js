6f56621ad063fd3a6b5e222a2a75284270f7335f
andyavt <andy.avtushenko@gmail.com>
2015-02-18 21:15:24 -0800

    Web_docs

---
+++ main.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($){
+
+/*
+	$(".menu a").click(function() {
+
+	
+		var href = $(this).attr("href");
+		cargarContenido(href);
+		
+		return false;
+	});
+        
+	
+	
+	function cargarContenido(pagina) {
+	
+		$('#content').animate({opacity:0}, 300,function(){
+		  		
+			 	$("#loadimage").show(); // show a loading image
+				// load content when opacity = 0 animation finished
+				$("#content").load(
+					pagina, 
+					function() {
+						$("#loadimage").hide(); // hide a loading image
+						// make opacity = 1 when content has been loaded
+						$('#content').animate({ "opacity": "1" });
+					});
+					
+				});
+				
+			
+				
+				
+	}
+			
+
+	*/	  
+	
+
+});
